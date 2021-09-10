3aca2b99a0b2cf972c132cac3cccb58f2c9b157f
Pawel89J <85400956+Pawel89J@users.noreply.github.com>
2021-09-10 15:49:28 +0200

    first add

---
+++ exercise/Section4/src/AreaCalculator.java
@@ -0,0 +1,9 @@
+public class AreaCalculator {
+
+    public static double area (double radious) {
+        return (radious <0) ? -1 : radious * radious * Math.PI;
+    }
+    public static double area (double x, double y) {
+        return (x <0 || y <0) ? -1 : x * y;
+    }
+}
