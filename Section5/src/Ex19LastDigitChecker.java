23917deb4f64c58264e994ab1f133f61e7355731
Pawel89J <85400956+Pawel89J@users.noreply.github.com>
2021-09-15 14:57:47 +0200

    Add files via upload

---
+++ Section5/src/Ex19LastDigitChecker.java
@@ -0,0 +1,16 @@
+public class Ex19LastDigitChecker {
+
+    public static boolean isValid(int number) {
+        return (number > 9) && (number < 1001);
+    }
+
+    public static boolean hasSameLastDigit(int x, int y, int z) {
+        if (isValid(x) && isValid(y) && isValid(z)) {
+            x %= 10;
+            y %= 10;
+            z %= 10;
+            return (x == y) || (x == z) || (y == z);
+        }
+        return false;
+    }
+}
