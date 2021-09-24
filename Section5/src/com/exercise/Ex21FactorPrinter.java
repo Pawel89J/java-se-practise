0fa873b85d06b286e0df27a2b58a3bca34135be8
Pawel89J <85400956+Pawel89J@users.noreply.github.com>
2021-09-24 14:51:57 +0200

    Edited section 5

---
+++ Section5/src/com/exercise/Ex21FactorPrinter.java
@@ -0,0 +1,18 @@
+package com.exercise;
+
+public class Ex21FactorPrinter {
+
+public static void printFactors(int number) {
+    if (number < 1) {
+        System.out.println("Invalid Value");
+        return;
+    }
+    int i = 1;
+    while (i <= number) {
+        if (number % i == 0){
+            System.out.print(i + " ");
+        }
+        i++;
+    }
+}
+}
